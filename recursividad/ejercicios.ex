0dabe0e091118e6f08463ac797b198ebe228212c
Carlos Andrés Florez <caflorez@uniquindio.edu.co>
2024-06-06 09:50:33 -0500

    Implementación iterativa fibonacci

--- recursividad/ejercicios.ex
+++ recursividad/ejercicios.ex
@@ -1,18 +1,39 @@
 defmodule Fibonacci do
 
-  def calcular_fibonacci(0), do: 0
-
-  def calcular_fibonacci(1), do: 1
-
   def calcular_fibonacci(n) when n < 0 do
     {:error, "El número debe ser no negativo"}
   end
+
+  def calcular_fibonacci(0), do: 0
+  def calcular_fibonacci(1), do: 1
 
   def calcular_fibonacci(n) when n > 1 do
     calcular_fibonacci(n - 1) + calcular_fibonacci(n - 2)
   end
 
 end
+
+defmodule FibonacciMejorado do
+
+  def calcular_fibonacci(n) when n < 0 do
+    {:error, "El número debe ser no negativo"}
+  end
+
+  def calcular_fibonacci(0), do: 0
+  def calcular_fibonacci(1), do: 1
+
+  def calcular_fibonacci(n) do
+    calcular_fibonacci(n, 0, 1)
+  end
+
+  defp calcular_fibonacci(0, a, _), do: a
+
+  defp calcular_fibonacci(n, a, b) do
+    calcular_fibonacci(n - 1, b, a + b)
+  end
+
+end
+
 
 defmodule Main do
   def run do
@@ -23,7 +44,7 @@ defmodule Main do
     case Integer.parse(user_input) do
       :error -> IO.puts("Debes introducir un número entero")
       {n, _} ->
-        case Fibonacci.calcular_fibonacci(n) do
+        case FibonacciMejorado.calcular_fibonacci(n) do
           {:error, message} -> IO.puts(message)
           result -> IO.puts("El resultado es: #{result}")
         end
