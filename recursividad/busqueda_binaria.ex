87c4476bf5ec29c9e36c702a30c292a1e0c71f8b
Carlos Andrés Florez <caflorez@uniquindio.edu.co>
2024-06-06 20:43:53 -0500

    Implementación búsqueda binaria

---
+++ recursividad/busqueda_binaria.ex
@@ -0,0 +1,49 @@
+defmodule BusquedaBinaria do
+
+  @moduledoc """
+    Módulo que implementa la búsqueda binaria. La búsqueda binaria es un algoritmo de búsqueda eficiente que permite encontrar un valor en una lista ordenada.
+  """
+
+  @doc """
+    Ordena una lista de enteros.
+  """
+  def ordenar(lista) do
+    lista
+    |> Enum.sort()
+  end
+
+  @doc """
+    Busca un valor en una lista ordenada.
+  """
+  def buscar(lista, valor) do
+    lista_ordenada = lista |> ordenar()
+    buscar(lista_ordenada, valor, 0, length(lista_ordenada) - 1)
+  end
+
+  defp buscar(lista, valor, inicio, fin) when inicio <= fin do
+    medio = div(inicio + fin, 2)
+
+    case Enum.at(lista, medio) do
+      x when x == valor -> true
+      x when x > valor -> buscar(lista, valor, inicio, medio - 1)
+      _ -> buscar(lista, valor, medio + 1, fin)
+    end
+
+  end
+
+  defp buscar(_lista, _valor, inicio, fin) when inicio > fin, do: false
+
+end
+
+defmodule Main do
+
+  def run do
+
+    respuesta = BusquedaBinaria.buscar( [2,3,7,9,1], 21 )
+    IO.puts(respuesta)
+
+  end
+
+end
+
+Main.run()
