fa6f60c3f828a5995ad03f7b315b8a38e9e5d32f
Carlos Andrés Flórez <caflorez@uniquindio.edu.co>
2024-06-11 20:45:01 -0500

    Nuevo ejemplo de concurrencia versión 2

--- concurrencia/ejemplo2.ex
+++ concurrencia/ejemplo2.ex
@@ -14,15 +14,11 @@ defmodule BusquedaConcurrente do
   def buscar_numero(lista, numero) do
 
     # Se divide la lista en cuatro partes
-    [parte1, parte2, parte3, parte4] = dividir_lista(lista)
+    partes = Enum.chunk_every(lista, div(length(lista), 4))
 
-    # Se crean cuatro tareas que buscarán el número en cada parte de la lista
-    tareas = [
-      Task.async( fn -> buscar_parte(parte1, numero) end ),
-      Task.async( fn -> buscar_parte(parte2, numero) end ),
-      Task.async( fn -> buscar_parte(parte3, numero) end ),
-      Task.async( fn -> buscar_parte(parte4, numero) end ),
-    ]
+    # Por cada parte se crea una tarea que buscará el número
+    tareas = partes
+    |> Enum.map( fn parte -> Task.async( fn -> buscar_parte(parte, numero) end ) end )
 
     # Se espera a que todas las tareas finalicen y se obtiene el resultado de cada una
     resultado = tareas |> Enum.map( &Task.await/1 )
@@ -33,38 +29,17 @@ defmodule BusquedaConcurrente do
   end
 
   @doc """
-    Divide una lista en cuatro partes. La última parte puede tener más elementos si la división no es exacta.
-  """
-  def dividir_lista(lista) do
-
-    # Se obtiene el tamaño de la lista y se calcula el tamaño de cada parte
-    tamano = length(lista)
-    parte = div(tamano, 4)
-
-    # Se obtiene cada parte de la lista
-    parte1 = Enum.slice(lista, 0, parte)
-    parte2 = Enum.slice(lista, parte, parte)
-    parte3 = Enum.slice(lista, 2*parte, parte)
-    parte4 = Enum.slice(lista, 3*parte, parte + rem(tamano, 4))
-
-    # Se devuelven las partes de la lista en una lista
-    [parte1, parte2, parte3, parte4]
-  end
-
-  @doc """
     Busca un número en una parte de una lista. Devuelve true si el número se encuentra en la parte.
   """
-  def buscar_parte(parte, numero) do
-    Enum.member?(parte, numero)
-  end
+  def buscar_parte(parte, numero), do: Enum.member?(parte, numero)
 
 end
 
 defmodule Main do
 
   def run do
-    numero = 10000001
-    respuesta = BusquedaConcurrente.crear_lista(10000001)
+    numero = 2
+    respuesta = BusquedaConcurrente.crear_lista(10000000)
     |> BusquedaConcurrente.buscar_numero(numero)
 
     IO.puts("¿Se ha encontrado el número #{numero}? #{respuesta}")
