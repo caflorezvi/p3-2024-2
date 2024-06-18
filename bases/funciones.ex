379cf12ce1ddc136529606aa8ae6a01b94bdd5a9
Carlos Andrés Flórez <caflorez@uniquindio.edu.co>
2024-06-17 20:35:53 -0500

    Ejercicios básicos del lenguaje

---
+++ bases/funciones.ex
@@ -0,0 +1,175 @@
+# Funcines anónimas
+
+suma = fn a, b -> a+b end # fn es una palabra reservada para definir una función anónima
+# suma = fn (a, b) -> a+b end # También se puede definir así
+IO.puts( suma.(1, 2) ) # 3
+
+# Función anónima usando el operador &
+
+suma = &(&1 + &2) # &1 y &2 son los argumentos de la función anónima
+IO.puts( suma.(1, 2) ) # 3
+
+IO.puts("--------------------")
+
+# Funciones con nombre
+
+defmodule Funciones do
+
+  @moduledoc """
+    Módulo que contiene funciones matemáticas. Un módulo es un contenedor de funciones y datos.
+  """
+  @doc """
+    Función que suma dos números. La última expresión de una función es su valor de retorno.
+  """
+  def suma(a, b) do
+    a + b
+  end
+
+  @doc """
+    Función que devuelve un saludo con el nombre que se le pase como argumento. Se usa la concatenación de cadenas.
+  """
+  def saludo_v1(nombre) do
+    "Hola " <> nombre
+  end
+
+  @doc """
+    Función que devuelve un saludo con el nombre que se le pase como argumento. Se usa función en una línea, para ello se usa la palabra clave do:.
+  """
+  def saludo_v2(nombre), do: "Hola " <> nombre
+
+  @doc """
+    Función que divide dos números y devuelve el cociente y el residuo. Se usa la función div y rem respectivamente. Se usa la tupla para devolver dos valores.
+  """
+  def dividir_v1(a, b) do
+    cociente = div(a, b)
+    residuo = rem(a, b)
+    {cociente, residuo}
+  end
+
+  @doc """
+    Función que divide dos números y devuelve el cociente y el residuo. Se usa el pattern matching para devolver un error si el divisor es cero.
+  """
+  def dividir_v2(a, b) do
+    case b do
+      0 -> {:error, "No se puede dividir por cero"}
+      _ -> {:ok, {div(a, b), rem(a, b)}}
+    end
+  end
+
+  @doc """
+    Función que devuelve un saludo con el nombre que se le pase como argumento. Se usa la interpolación de cadenas.
+  """
+  def saludo_jornada(nombre) do
+    # Se obtiene la hora actual con la función local_time del módulo Calendar.
+    hora_actual = :calendar.local_time
+    # Se usa el pattern matching para obtener la hora actual y se usa el case para devolver un saludo según la hora.
+    case hora_actual do
+      {_, {hora, _, _}} when hora >= 0 and hora < 12 -> "Buenos días #{nombre}"
+      {_, {hora, _, _}} when hora >= 12 and hora < 19 -> "Buenas tardes #{nombre}"
+      {_, {hora, _, _}} when hora >= 19 and hora < 24 -> "Buenas noches #{nombre}"
+    end
+  end
+
+  @doc """
+    Función que pide un valor al usuario y lo muestra en pantalla. Se usa la función IO.gets para obtener el valor y la función IO.puts para mostrarlo.
+  """
+  def ingresar_valor do
+    # Se usa la función IO.gets para obtener el valor y la función String.trim para quitar los espacios en blanco. El operador |> se usa para pasar el resultado de una función a otra.
+    valor = IO.gets("Ingrese un valor: ") |> String.trim()
+    IO.puts("El valor ingresado es: #{valor}")
+  end
+
+  @doc """
+    Función que indica el rango de edad según la edad que se le pase como argumento. Se usa cond para definir condiciones.
+  """
+  def indicar_rango_edad_case(edad) do
+    cond do
+      edad >= 0 and edad < 18 -> "Menor de edad"
+      edad >= 18 and edad < 65 -> "Mayor de edad"
+      edad >= 65 -> "Adulto mayor"
+    end
+  end
+
+  @doc """
+    Función que indica el rango de edad según la edad que se le pase como argumento. Se usa el pattern matching para devolver un rango de edad. Se usa la palabra clave when para definir condiciones.
+  """
+  def indicar_rango_edad(edad) when edad >= 0 and edad < 18 do
+    "Menor de edad"
+  end
+
+  def indicar_rango_edad(edad) when edad >= 18 and edad < 65 do
+    "Mayor de edad"
+  end
+
+  def indicar_rango_edad(edad) when edad >= 65 do
+    "Adulto mayor"
+  end
+
+end
+
+defmodule Loteria do
+  @moduledoc """
+    Módulo que contiene funciones de ejemplo. Una función privada no puede ser llamada desde fuera del módulo. Se usa la palabra clave defp para definir una función privada.
+  """
+
+  @doc """
+    Función que valida si un número ingresado por el usuario es igual al número de la lotería. Se usa la función leer_numero para obtener el número ingresado por el usuario. Se usa el pattern matching para comparar el número ingresado con el número de la lotería.
+  """
+  def validar_numero do
+    loteria = :rand.uniform(100)
+    numero = leer_numero()
+
+    # Se usa el pattern matching para comparar el número ingresado con el número de la lotería.
+    case numero do
+      ^loteria -> IO.puts("¡Felicidades! Has ganado la lotería.")
+      _ -> IO.puts("Lo siento, no has ganado la lotería. El número ganador era #{loteria}.")
+    end
+  end
+
+  defp leer_numero do
+
+    # Se usa la función IO.gets para obtener el valor y la función String.trim para quitar los espacios en blanco. El operador |> se usa para pasar el resultado de una función a otra.
+    valor = "Ingrese un número entre 0 y 100: " |> IO.gets |> String.trim
+
+    # Se usa la función Integer.parse para convertir el valor a un número entero. El case se usa para hacer coincidencia de patrones.
+    case Integer.parse(valor) do
+      {n, _} when n in 0..100 ->
+        n
+      {n, _} ->
+        IO.puts("El número #{n} no es válido, debe estar entre 0 y 100.")
+        leer_numero()
+      :error ->
+        IO.puts("El valor ingresado no es un número válido.")
+        leer_numero()
+    end
+  end
+
+end
+
+# Llamando a la función suma del módulo Funciones
+IO.puts( Funciones.suma(1, 2) ) # 3
+
+# Llamando a la función saludo_v1 del módulo Funciones
+IO.puts( Funciones.saludo_v1("Carlos") ) # Hola Carlos
+
+# Llamando a la función de dividir del módulo Funciones usando pattern matching. El patrón {cociente, residuo} se usa para desestructurar la tupla que devuelve la función.
+{cociente, residuo} = Funciones.dividir_v1(10, 3)
+IO.puts( "El cociente de la división de 10/3 es: #{cociente} y el residuo es #{residuo}") # 3
+
+# Llamando a la función de dividir del módulo Funciones usando pattern matching. El patrón {:ok, {cociente, residuo}} se usa para desestructurar la tupla que devuelve la función.
+case Funciones.dividir_v2(10, 0) do
+  {:ok, {cociente, residuo}} -> IO.puts( "El cociente de la división de 10/3 es: #{cociente} y el residuo es #{residuo}")
+  {:error, mensaje} -> IO.puts( mensaje )
+end
+
+# Llamando a la función saludo_jornada del módulo Funciones
+IO.puts( Funciones.saludo_jornada("Carlos") ) # Buenos días Carlos
+
+# Llamando a la función ingresar_valor del módulo Funciones
+Funciones.ingresar_valor
+
+# Llamando a la función indicar_rango_edad del módulo Funciones
+IO.puts( Funciones.indicar_rango_edad(10) ) # Menor de edad
+
+# Llamando a la función validar_numero del módulo Loteria
+Loteria.validar_numero
---
+++ bases/inicio.ex
@@ -0,0 +1,96 @@
+# En Elixir, la inmutabilidad es una característica fundamental. Esto significa que una vez que se asigna un valor a una variable, no se puede cambiar. En lugar de modificar el valor de una variable, se crea una nueva variable con el nuevo valor. Esto es útil para evitar errores y facilitar la depuración del código.
+# En Elixir, las variables se asignan con el operador =. Las variables pueden contener cualquier tipo de dato, como enteros, flotantes, cadenas de texto, booleanos, listas, tuplas, mapas, etc.
+
+# Tipos de datos básicos
+
+# Números
+
+# En Elixir, los números son enteros o flotantes. Los enteros son números sin decimales y los flotantes son números con decimales. Los números enteros pueden ser positivos o negativos. Los flotantes pueden ser positivos o negativos y pueden tener decimales. Los números enteros y flotantes son de precisión arbitraria, lo que significa que no hay un límite en el tamaño de los números enteros o flotantes. Los números enteros y flotantes se pueden representar en notación decimal, hexadecimal, octal y binaria.
+a = 1 # Entero
+b = 3.0 # Flotante
+
+# Operaciones aritméticas
+c = a + b # Suma
+d = a - b # Resta
+e = a * b # Multiplicación
+f = a / b # División
+g = rem(a, 2) # Resto
+h = div(a, 2) # Cociente
+
+# Cadena de texto
+
+# En Elixir, las cadenas de texto son listas de caracteres. Los caracteres son enteros que representan un carácter Unicode. Las cadenas de texto se pueden definir con comillas dobles o comillas simples. Las cadenas de texto se pueden concatenar con el operador <>.
+
+nombre = "Juan"
+apellido = "Pérez"
+nombre_completo = nombre <> " " <> apellido # "Juan Pérez" con concatenación de cadenas
+nombre_completo = "#{nombre} #{apellido}" # "Juan Pérez" con interpolación de cadenas
+
+# String es un módulo que contiene funciones para trabajar con cadenas de texto. Algunas funciones útiles del módulo String son upcase, downcase, capitalize, reverse, length, slice, split, etc.
+
+nombre_mayusculas = String.upcase(nombre) # "JUAN"
+nombre_minusculas = String.downcase(nombre) # "juan"
+nombre_capitalizado = String.capitalize(nombre) # "Juan"
+nombre_invertido = String.reverse(nombre) # "nauJ"
+longitud_nombre = String.length(nombre) # 4
+subcadena_nombre = String.slice(nombre, 0..2) # "Jua"
+nombre_separado = String.split(nombre, "u") # ["J", "an"]
+
+# Booleanos
+
+# En Elixir, los booleanos son valores que representan verdadero o falso. Los booleanos son útiles para controlar el flujo de un programa. Los booleanos se pueden definir con las palabras clave true y false.
+
+a = true
+b = false
+
+#Operadores lógicos
+
+# En Elixir, los operadores lógicos son útiles para combinar o invertir valores booleanos. Los operadores lógicos son and, or y not. El operador and devuelve true si ambos operandos son true. El operador or devuelve true si al menos uno de los operandos es true. El operador not devuelve true si el operando es false.
+
+c = a and b # false
+d = a or b # true
+e = not a # false
+
+# Operadores de comparación
+
+a = 1
+b = 2
+
+c = a == b # Igual a
+d = a != b # Diferente de
+e = a < b # Menor que
+f = a <= b # Menor o igual que
+g = a > b # Mayor que
+h = a >= b # Mayor o igual que
+
+# Operador pipe. El operador pipe |> se utiliza para encadenar funciones en Elixir. El operador pipe toma el resultado de la expresión a su izquierda y lo pasa como primer argumento a la función a su derecha. Esto permite escribir código de manera más legible y concisa.
+
+# Ejemplo 1 de uso del operador pipe
+
+a = 1
+b = 2
+c = 3
+
+resultado = a + b |> div(c) # Equivalente a div(a + b, c)
+
+# Ejemplo 2 de uso del operador pipe
+
+nombre = "Juan Pérez"
+nombre_mayusculas = nombre |> String.upcase() # Equivalente a String.upcase(nombre)
+
+# Ejemplo 3 de uso del operador pipe
+
+"Lenguaje de programación Elixir"  |> String.upcase() |> String.split() # ["LENGUAJE", "DE", "PROGRAMACIÓN", "ELIXIR"]
+
+# Operador ^. El operador ^ se utiliza para acceder a variables en patrones de coincidencia. El operador ^ se utiliza para evitar la reasignación de variables en patrones de coincidencia. El operador ^ se utiliza para acceder a variables en patrones de coincidencia.
+
+# Asignamos un valor a la variable x
+x = 10
+
+# Usamos el operador ^ para hacer coincidir el valor de x en el patrón. Case es una estructura de control que permite hacer coincidencias de patrones.
+case {10, 20} do
+  {^x, y} ->
+    IO.puts("Coincidió con el valor de x: #{x}, y el valor de y es: #{y}")
+  _ ->
+    IO.puts("No coincidió")
+end
